dfc5db3101c28829430750b3dd8fe4b638dc3f2f
le-as-a <leasia0817@gmail.com>
2024-02-19 12:31:18 -0600

    removed example seed data

--- db/setup.py
+++ db/setup.py
@@ -1,5 +1,5 @@
 from sqlite3 import connect
-from helper import schemas, callings, graveyard, characters, abilities, char_abilities, species_abilities, quirks # data is private due to copyright of BREAK!! RPG system
+from helper import schemas, callings, graveyard, abilities, species_abilities, quirks # data is private due to copyright of BREAK!! RPG system
 
 conn = connect("data.db")
 cur = conn.cursor()
@@ -17,10 +17,10 @@ for x in schemas:
     
 cur.executemany("INSERT INTO callings VALUES (?,?,?,?,?,?,?)", callings)
 cur.executemany("INSERT INTO graveyard VALUES (?,?,?,?,?,?)", graveyard)
-cur.executemany("INSERT INTO characters VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", characters)
+# cur.executemany("INSERT INTO characters VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", characters)
 cur.executemany("INSERT INTO abilities VALUES (?,?,?,?)", abilities)
 cur.executemany("INSERT INTO abilities VALUES (?,?,?,?)", species_abilities)
-cur.executemany("INSERT INTO character_abilities VALUES (?,?,?,?)", char_abilities)
+# cur.executemany("INSERT INTO character_abilities VALUES (?,?,?,?)", char_abilities)
 cur.executemany("INSERT INTO quirks VALUES(?,?,?)", quirks)
 conn.commit()
 conn.close()
